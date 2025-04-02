7c5e46e584f24745a4d9129e409225d67737f587
Developer-Ronnie <ronnieworkspace@gmail.com>
2025-04-03 01:18:03 +0530

    Initial website layout

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Shimmerluxe'
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Shinning Moments, Shinning You!'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION ||'Luxury, Affordable & Handcrafted Jewelry for Every Occasion Buy Premium Rings, Necklaces & Bracelets Online with Free Shipping & Exclusive Designs.'
