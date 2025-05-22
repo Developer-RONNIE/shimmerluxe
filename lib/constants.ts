e93e6f541af32966a0c36357c9f64d32280f77f9
Developer-Ronnie <ronnieworkspace@gmail.com>
2025-05-22 08:41:47 +0530

    09-create-product-details-page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Shimmerluxe'
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Shinning Moments, Shinning You!'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION ||'Luxury, Affordable & Handcrafted Jewelry for Every Occasion Buy Premium Rings, Necklaces & Bracelets Online with Free Shipping & Exclusive Designs.'
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
